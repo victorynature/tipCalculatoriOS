2a07030614e208a71251272470dc626545937242
Jiyoung Park <cs.jiyoung.park@gmail.com>
2013-09-20 14:40:22 -0700

    Importing my project to Gui, without saving history.

---
+++ tipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipCalculator
+//
+//  Created by Jiyoung Park on 9/19/13.
+//  Copyright (c) 2013 Jiyoung Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipCalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipCalculator
+//
+//  Created by Jiyoung Park on 9/19/13.
+//  Copyright (c) 2013 Jiyoung Park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
